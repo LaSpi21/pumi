88b72c7dd8ae7258dc7f0831ad0691dee92c2b9c
Pablo La Spina <63978144+LaSpi21@users.noreply.github.com>
2024-02-13 15:16:21 -0300

    Update manage_actions.sh

--- manage_actions.sh
+++ manage_actions.sh
@@ -11,24 +11,30 @@ while true; do
     clear
     echo "Pumi. Configurar acciones programadas."
     echo "Selecciona una acción:"
-    echo "1. Ver las acciones programadas"
-    echo "2. Borrar acciones programadas"
-    echo "3. Volver al menú principal"
-    
+    echo "1. Programar un cambio de imagen"
+    echo "2. Ver las acciones programadas"
+    echo "3. Borrar acciones programadas"
+    echo "4. Volver al menú principal"
+
     read opcion
     
     case $opcion in
+
         1)
+            sudo bash "$SCRIPT_DIR"/schedule.sh
+            ;;
+
+        2)
             sudo bash "$SCRIPT_DIR"/view_crontab.sh
             ;;
-        2)
+        3)
             sudo bash "$SCRIPT_DIR"/erase_crontab.sh
             ;;
-        3)
+        4)
            break
            ;;
         *)
-            echo "Opción no válida. Por favor, selecciona 1, 2 ó 3."
+            echo "Opción no válida. Por favor, selecciona 1, 2, 3 ó 4."
             ;;
     esac
 done    
