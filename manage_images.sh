ed03f1fde1f764244cd18af82a707457b90a6c4f
Pablo La Spina <63978144+LaSpi21@users.noreply.github.com>
2024-02-13 14:37:13 -0300

    Update manage_images.sh

--- manage_images.sh
+++ manage_images.sh
@@ -5,24 +5,27 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 RED='\033[0;31m'
 NC='\033[0m'
 
-echo "Selecciona una acción:"
-echo "1. Agregar nueva imagen"
-echo "2. Cambiar dirección del repositorio"
-echo "3. Volver"
+while true; do
+    echo "Selecciona una acción:"
+    echo "1. Agregar nueva imagen"
+    echo "2. Cambiar dirección del repositorio"
+    echo "3. Volver"
 
-read opcion
-
-case $opcion in
-    1)
-        echo "Agregando nueva imagen"
-        sudo bash "$SCRIPT_DIR"/add_image.sh
-        ;;
-    2)
-        echo "Agreando maquina/s"
-        sudo bash "$SCRIPT_DIR"/ssh_add.sh
-        ;;
-    3) 
-    *)
-        echo "Opción no válida. Por favor, selecciona 1, 2 o 3."
-        ;;
-esac
+    read opcion
+    
+    case $opcion in
+        1)
+            echo "Agregando nueva imagen"
+            sudo bash "$SCRIPT_DIR"/add_image.sh
+            ;;
+        2)
+            sudo bash "$SCRIPT_DIR"/change_repo_path.sh
+            ;;
+        3)
+            break
+            ;;
+        *)
+            echo "Opción no válida. Por favor, selecciona 1, 2 o 3."
+            ;;
+    esac
+done
