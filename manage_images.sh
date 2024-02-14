1c20a34c6f3b5aa46e8381f1e70ecbbe0daf8645
Pablo La Spina <63978144+LaSpi21@users.noreply.github.com>
2024-02-13 22:54:25 -0500

    Update manage_images.sh

--- manage_images.sh
+++ manage_images.sh
@@ -4,8 +4,19 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 RED='\033[0;31m'
 NC='\033[0m'
+first_iteration=true
 
 while true; do
+
+    # Check if it's not the first iteration
+    if [ "$first_iteration" != true ]; then
+        echo "Presione cualquier tecla para continuar"
+        read -n 1 -s -r -p ""
+    fi
+
+    # Set first_iteration to false after the first iteration
+    first_iteration=false
+
     clear
     echo "Pumi. Configurar imágenes"
     echo "Selecciona una acción:"
