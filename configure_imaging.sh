ff1519725beb5d1bbbdf641710f261ebf671724c
Pablo La Spina <63978144+LaSpi21@users.noreply.github.com>
2024-02-16 17:23:15 -0300

    Update configure_imaging.sh

--- configure_imaging.sh
+++ configure_imaging.sh
@@ -1,4 +1,6 @@
-v#!/bin/bash
+#!/bin/bash
+
+#Funcion que es llamada por install.sh para configurar la implementacion del envio de log por mail y la direccion del repositorio
 
 RED='\033[0;31m'
 NC='\033[0m'
@@ -10,6 +12,7 @@ echo Configurando el mail
 read -p "Ingresa el mail de uso (ejemplo@algo.com): " mail
 read -p "Ingresa la contraseña de aplicación (si no se tiene una, generarla): " pass
 
+#Lineas a agregar al archivo /etc/ssmtp/ssmtp.conf para habilitar el mail
 conf="UseSTARTTLS=YES
 FromLineOverride=YES
 root=admin@pumi.com
@@ -22,15 +25,17 @@ AuthPass="$pass""
 echo "$conf" | sudo tee -a /etc/ssmtp/ssmtp.conf > /dev/null
 
 
+#Indica la ruta del archivo para ubicar de forma relativa el resto
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 repo_path_file="$SCRIPT_DIR/Repo_path"
 repo_path=""
 
 
-
+#Pide la ubicacion absoluta del repositorio
 echo Configurando repositorio de imagenes
 read -e -p "Ingresa la ruta al repositorio (ej. /media/user/repo/)" repo_path
 
+#Completa el archivo /pumi/Repo_path con las rutas e uuids del repositorio y de la carpeta /pumi/
 echo "$repo_path" | sudo tee "$repo_path_file" > /dev/null
 
 ID_repo=$(sudo blkid -o value -s UUID $(\df --output=source "$repo_path"|tail -1))
@@ -43,6 +48,7 @@ echo "$ID_pumi" | sudo tee -a "$repo_path_file" > /dev/null
 
 echo "$mail" | sudo tee -a "$repo_path_file" > /dev/null
 
+#Agrega un tiempo limite por defecto de 45 minutos para finalizar el cambio de imagen.
 echo "45" | sudo tee -a "$repo_path_file" > /dev/null
 
 echo "pumi configurado, el tiempo maximo por defecto para clonar imagenes es 45 minutos, puede cambiarlo desde pumi>Configurar acciones programadas"
