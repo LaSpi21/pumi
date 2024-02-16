3fe877741f7419e4a2a2abf124d860581427a653
Pablo La Spina <63978144+LaSpi21@users.noreply.github.com>
2024-02-16 13:54:27 -0300

    Update change_timeout.sh

--- change_timeout.sh
+++ change_timeout.sh
@@ -9,9 +9,19 @@ NC='\033[0m'
 repo_path_file="$SCRIPT_DIR/Repo_path"
 minutos=""
 
-echo Configurando repositorio de imagenes
+echo "Cambiando el tiempo limite del cambio de imagen."
+
+validar_minuto() {
+  local min=$1
+  min=$(expr $min + 0)
+  if [[ ! "$min" =~ ^[0-9]+$ ]]; then
+    echo -e "${RED}Cantidad de minutos inválida. La cantidad debe ser un número entero.${NC}"
+    exit 1
+  fi
+}
 
 read -p "Ingresa el nuevo tiempo limite en minutos" minutos
 
+validar_minuto "$minutos"
 
 sed -i "5s|.*|$minutos|" "$repo_path_file"
