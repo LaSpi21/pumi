0edf561d6af75e6788631bfcbf5477e2450aec47
Pablo La Spina <63978144+LaSpi21@users.noreply.github.com>
2024-02-16 16:34:39 -0300

    Update Restore.sh
    Agrego comentarios

--- Restore.sh
+++ Restore.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+#Este archivo se corre al momento de comenzar una imagen
+#Esta encargado de programar un log para luego de realizar el cambio de imagen
+#Luego reinicia el servidor a la entrada de grub correspondiente para el cambio inidicado
+
+
+#Indica la ruta del archivo para ubicar de forma relativa el resto
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-# Check if the script is run with sudo
+# Toma el nombre de usuario por cuestiones de pathing
 if [ "$EUID" -ne 0 ]; then
     # Script is not running as root, use the current username
     user_script=$(id -u -n)
@@ -11,21 +17,29 @@ else
     user_script=$SUDO_USER
 fi
 
-increment=$(($(cat "$SCRIPT_DIR/Repo_path"| sed -n '5p') + 15))
+#increment es la cantidad de tiempo que espera clonezilla para reiniciarse en minutos, se encuentra en el archivo /pumi/Repo_path
+increment=$(($(cat "$SCRIPT_DIR/Repo_path"| sed -n '5p') + 30))
 
+#toma la dirección de los csvs que puede necesitar utilizar
 log_csv="$SCRIPT_DIR/log/log.csv"
 macs_csv="$SCRIPT_DIR/log/Macs.csv"
 new_csv="$SCRIPT_DIR/log/new.csv"
 fail_csv="$SCRIPT_DIR/log/failed.csv"
+
+#Variables para uso de opcionales
 failed=false
 new=false
+retry=false
+onetimeonly=false
+
+#placeholder para el nombre de la imagen
 image_name=""
+
+#toma el momento en el cual programar el log
 log_hour=$(date -d "+$increment minutes" "+%H")
 log_min=$(date -d  "+$increment minutes" "+%M")
 log_day=$(date -d  "+$increment minutes" "+%d")
 log_month=$(date -d  "+$increment minutes" "+%m")
-retry=false
-onetimeonly=false
 
 
 while getopts ":i:fnro" opt; do
@@ -52,8 +66,10 @@ while getopts ":i:fnro" opt; do
   esac
 done
 
+#linea base a agregar a crontab
 cron_line="$log_min $log_hour $log_day $log_month * bash $SCRIPT_DIR/log.sh"
 
+#pasa los opcionales recibidos a log.sh
 if [ "$retry" = true ]; then
   cron_line="$cron_line -r"
 fi
@@ -78,12 +94,12 @@ echo Se agregó "$cron_line"
 
 
 
-#Cambia la imagen y borra las firmas
+#Cambia la imagen que figura para cada maquina en log.csv y borra las firmas
 while IFS=, read -r mac ip_address serial user image sign; do
   awk -F, -v pattern="$mac" -v image="$image_name"  '$1 == pattern {$5=image;$6=""} 1' OFS="," "$log_csv" > temp.csv && mv temp.csv "$log_csv"
 done < "$log_csv"
 
-#copia las macs correspondientes
+#copia las macs de las maquinas que recibiran imagen, solo las que se encuentren en macs_csv se usaran
 if [ "$failed" = true ]; then
   sudo awk -F ',' '{print $1 "," $2}' "$fail_csv" | sudo tee "$macs_csv" > /dev/null
 
@@ -97,13 +113,9 @@ sudo awk -F ',' '{print $1 "," $2}' "$log_csv" | sudo tee "$macs_csv" > /dev/nul
 
 fi
 
-
+#Indica la entrada de grub a utilizar y reinicia 
 source /home/"$user_script"/.bashrc
-# Set grub to boot to the unattended backup
-#echo sudo /usr/sbin/grub-reboot "Restore $image_name"
 sudo /usr/sbin/grub-reboot "Restore $image_name"
 sleep 2
-#Reboot server
 /sbin/reboot
-#sudo /sbin/shutdown -r now
 
