43f520e769b834e166c5bff6e3dd17040d2ef29a
Pablo La Spina <63978144+LaSpi21@users.noreply.github.com>
2024-02-16 13:48:21 -0300

    Update change_timeout.sh

--- change_timeout.sh
+++ change_timeout.sh
@@ -1,2 +1,17 @@
 #/bin/bash
 
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+BLUE='\033[0;34m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+repo_path_file="$SCRIPT_DIR/Repo_path"
+minutos=""
+
+echo Configurando repositorio de imagenes
+
+read -p "Ingresa el nuevo tiempo limite en minutos" minutos
+
+
+sed -i "5s|.*|$minutos|" "$repo_path_file"
