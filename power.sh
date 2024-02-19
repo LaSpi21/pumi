f7a50960f72c33c1fe853587c98a9563980bf794
Pablo La Spina <63978144+LaSpi21@users.noreply.github.com>
2024-02-19 13:12:42 -0300

    Update power.sh

--- power.sh
+++ power.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+
+#Indica la ruta del archivo para ubicar de forma relativa el resto
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
+#toma la ruta del archivo log
 log_csv="$SCRIPT_DIR/log/log.csv"
 
+#instacia el valor de power_type
 power_type="c"
 
+#pide si se requieren encender o apagar las computadoras
 read -p "Encender o apagar computadoras? [e/a]: " power_type
 
-
+#funcion para despertar a todos los nodos
 wake() {
 #    local mac=$1
 #    local ip_address=$2
@@ -18,6 +23,7 @@ wake() {
     wakeonlan "$mac"
 }
 
+funcion para apagar todos los nodos
 shutdown(){
     echo apagando "$mac"
     sudo ssh -n "$user@$ip_address" "sudo shutdown now"
@@ -25,7 +31,7 @@ shutdown(){
 }
 
 
-
+#Apagar o encender dependiendo del valor ingresado para power_type
 if [[ "$power_type" == "e" ]]; then
 	while IFS=, read -r mac ip_address serial user image sign; do
 	    wake "$mac" "$ip_address" "$serial" "$user" &
@@ -38,6 +44,7 @@ elif [[ "$power_type" == "a" ]]; then
 
 fi
 
+#como esta accion esta fuera de todo submenú, se añade este separador
 echo "Presione cualquier tecla para continuar"
 read -n 1 -s -r -p ""
 
