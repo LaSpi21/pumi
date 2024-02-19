b869c37a7404479cd85660626272456f61a463db
Pablo La Spina <63978144+LaSpi21@users.noreply.github.com>
2024-02-19 13:01:37 -0300

    Update manage_images.sh

--- manage_images.sh
+++ manage_images.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 
+#Menú para configurar las imagenes y el repositorio.
+
+#Indica la ruta del archivo para ubicar de forma relativa el resto
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 RED='\033[0;31m'
 NC='\033[0m'
+
+
+
 first_iteration=true
 
+#
 while true; do
 
-    # Check if it's not the first iteration
+    # en caso de no ser la primera iteración
     if [ "$first_iteration" != true ]; then
         echo "Presione cualquier tecla para continuar"
         read -n 1 -s -r -p ""
     fi
 
-    # Set first_iteration to false after the first iteration
+    # Despues de la primera iteración, indicar que esta ya sucedió
     first_iteration=false
+
+    #Menu simple utilizando opciones
 
     clear
     echo "Pumi. Configurar imágenes"
