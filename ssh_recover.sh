2a04d2becfe6d486fc66eb76a14f68616a651da8
Pablo La Spina <63978144+LaSpi21@users.noreply.github.com>
2024-02-26 03:01:25 -0300

    Update ssh_recover.sh

--- ssh_recover.sh
+++ ssh_recover.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+#Recupera maquinas que hayan sido borradas y aun se encuentren en el .csv de reciclaje
+
+#Indica la ruta del archivo para ubicar de forma relativa el resto
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
+#toma la ruta de los distintos .csv que requiere
 log_csv="$SCRIPT_DIR/log/log.csv"
 recycle_csv="$SCRIPT_DIR/log/recycle.csv"
 new_csv="$SCRIPT_DIR/log/new.csv"
@@ -58,7 +62,7 @@ if [[ "$opcion" == "m" ]]; then
 	done
 
 
-
+    #Agrega la maquina de MAC indicada desde el reciclaje al log y a new, la borra del reciclaje
     sudo awk -F, -v pattern=$MAC '$1 == pattern' OFS="," "$recycle_csv" >>  "$log_csv"
     sudo awk -F, -v pattern=$MAC '$1 != pattern' OFS="," "$recycle_csv" > temp.csv && sudo mv temp.csv "$recycle_csv"
     sudo awk -F, -v pattern=$MAC '$1 != pattern' OFS="," "$new_csv" > temp.csv && sudo mv temp.csv "$new_csv"
@@ -79,7 +83,7 @@ if [[ "$opcion" == "i" ]]; then
             fi
         done
 
-
+    #Agrega la maquina de IP indicada desde el reciclaje al log y a new, la borra del reciclaje
     sudo awk -F, -v pattern=$IP '$2 == pattern' OFS="," "$recycle_csv" >>  "$log_csv"
     sudo awk -F, -v pattern=$IP '$2 != pattern' OFS=","  "$recycle_csv" > temp.csv && sudo mv temp.csv  "$recycle_csv"
     sudo awk -F, -v pattern=$IP '$2 != pattern' OFS="," "$new_csv" > temp.csv && sudo mv temp.csv "$new_csv"
