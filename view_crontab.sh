a0e39cceeb052dfcb6aebf3cba13df1215990838
Pablo La Spina <63978144+LaSpi21@users.noreply.github.com>
2024-02-26 02:58:55 -0300

    Update view_crontab.sh

--- view_crontab.sh
+++ view_crontab.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+#Simplemente muestra en pantalla desde pumi a las acciones programadas en crontab
+
+#Indica la ruta del archivo para ubicar de forma relativa el resto
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 RED='\033[0;31m'
 NC='\033[0m'
 
+#imprime todas las entradas que no se encuentren comentadas, ToDo: dar formato amigable
 cat <(sudo crontab -l | grep -v '^#' | awk NF)
 
