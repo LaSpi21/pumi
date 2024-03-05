54422cd74313754aaf7da9c2c10c8a51832444e9
Pablo La Spina <63978144+LaSpi21@users.noreply.github.com>
2024-03-05 11:29:00 -0300

    Update network_scan.sh

--- network_scan.sh
+++ network_scan.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+get_network() {
+    local default_interface=$(ip route | awk '/default/ {print $5}')
+    local local_ip=$(ip -o -4 addr show dev "$default_interface" | awk -F 'inet ' '{split($2, a, "/"); print a[1]}') 
+    local network=$(echo "$local_ip" | awk -F. '{print $1"."$2"."$3".0/24"}')
+    echo "$network"
+}
+network=$(get_network)
+
 read -p "Ingresa el usuario de las imagenes: " usuario
-sudo nmap -sn 10.1.181.0/24 | grep -E "Nmap scan|MAC Address" | awk '/Nmap/{ip=$NF} /MAC Address/{print $3 "," ip}' | grep -vP '\([^)]*\)' | nl -s, -w1 | awk -v user="$usuario"  -F',' '{print $2 "," $3 "," $1 "," user "," ","}'
+sudo nmap -sn $network | grep -E "Nmap scan|MAC Address" | awk '/Nmap/{ip=$NF} /MAC Address/{print $3 "," ip}' | grep -vP '\([^)]*\)' | nl -s, -w1 | awk -v user="$usuario"  -F',' '{print $2 "," $3 "," $1 "," user "," ","}'
 
 
