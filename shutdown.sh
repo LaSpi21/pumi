e9e488297328cc125ab987636c29d71fcff9e252
Pablo La Spina <63978144+LaSpi21@users.noreply.github.com>
2024-03-05 22:24:39 -0300

    Create shutdown.sh

---
+++ shutdown.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Indica la ruta del archivo para ubicar de forma relativa el resto
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+#toma la ruta del archivo log
+log_csv="$SCRIPT_DIR/log/log.csv"
+
+funcion para apagar todos los nodos
+shutdown(){
+    echo apagando "$mac"
+    sudo ssh -n "$user@$ip_address" "sudo shutdown now"
+}
+
+while IFS=, read -r mac ip_address serial user image sign; do
+    shutdown "$mac" "$ip_address" "$serial" "$user" &
+done < "$log_csv"
