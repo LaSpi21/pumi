954b29db920bd40e25470c1f95135a001f82cd45
Pablo La Spina <63978144+LaSpi21@users.noreply.github.com>
2024-03-27 11:11:36 -0300

    Update configure_imaging.sh

--- configure_imaging.sh
+++ configure_imaging.sh
@@ -54,6 +54,6 @@ echo "100" | sudo tee -a "$repo_path_file" > /dev/null
 #Agrega la contraseña de pumi
 read -s -p "Indica la contraseña de Pumi: " p
 hash=$(echo -n "$pw" | md5sum | cut -d ' ' -f 1)
-echo "$hash" | sudo tee "$repo_path_file" > /dev/null
+echo "$hash" | sudo tee -a "$repo_path_file" > /dev/null
 
 echo "pumi configurado, el tiempo maximo por defecto para clonar imagenes es 100 minutos, puede cambiarlo desde pumi>Configurar acciones programadas"
