8e3369537f179f4cc45355c479b2ca9034c3e654
Pablo La Spina <63978144+LaSpi21@users.noreply.github.com>
2024-03-27 11:04:29 -0300

    Update configure_imaging.sh

--- configure_imaging.sh
+++ configure_imaging.sh
@@ -47,6 +47,11 @@ echo "$ID_pumi" | sudo tee -a "$repo_path_file" > /dev/null
 
 echo "$mail" | sudo tee -a "$repo_path_file" > /dev/null
 
+#Agrega la contraseña de pumi
+read -s -p "Indica la contraseña de Pumi: " p
+hash=$(echo -n "$pw" | md5sum | cut -d ' ' -f 1)
+echo "$hash" | sudo tee "$repo_path_file" > /dev/null
+
 #Agrega un tiempo limite por defecto de 100 minutos para finalizar el cambio de imagen.
 echo "100" | sudo tee -a "$repo_path_file" > /dev/null
 
