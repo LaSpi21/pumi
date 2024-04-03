9280a660cfe724cea9e7e20e2754447ce3889e12
Pablo La Spina <63978144+LaSpi21@users.noreply.github.com>
2024-04-03 12:23:48 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ sudo chmod +x /usr/local/bin/pumi
 
 
 # Define dependencias
-dependencies=("crontab" "expect" "wakeonlan" "grub2" "ssh" "openssh-client" "ssmtp" "mpack" "iptables-persistent" "sshpass" "wget" "awk" "nmap")
+dependencies=("crontab" "expect" "wakeonlan" "grub2" "ssh" "openssh-client" "ssmtp" "mpack" "iptables-persistent" "sshpass" "wget" "awk" "jq" "nmap")
 
 # Instala dependencias en caso de no estar instaladas previamente
 for dep in "${dependencies[@]}"; do
