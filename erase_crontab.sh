58dcc5462924721caa098bf6cf60ae8b058a6a9c
Pablo La Spina <63978144+LaSpi21@users.noreply.github.com>
2024-04-15 14:19:44 -0300

    Update erase_crontab.sh

--- erase_crontab.sh
+++ erase_crontab.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 confirm=n
+
+cat <(sudo crontab -l | grep -v '^#' | awk NF) | nl
+
 read -p "Indique que acciones borrar, ingrese T para borrar TODAS las acciones, un número para borrar la acción de la línea que corresponda a ese número o c para cancelar [t/n°/c]: " confirm_input
 
 case $confirm_input in
